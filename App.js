9b5f662e30a9268b175c27e36f24a4a97ee6ede9
namakandoakende-coder <namakandoakende@gmail.com>
2026-06-30 20:38:15 +0200

    Initialize App with navigation and screens

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SplashScreen from "./src/screens/SplashScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import ChatScreen from "./src/screens/ChatScreen";
+import AdminScreen from "./src/screens/AdminScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="Admin" component={AdminScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
